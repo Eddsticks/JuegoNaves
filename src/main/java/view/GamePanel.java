24e1d820e7d5ce25e045e484dd67fb027fccfd53
Eduardo <ewaldo.jefe@gmail.com>
2025-07-23 01:15:03 -0400

    fondo negro

--- src/main/java/view/GamePanel.java
+++ src/main/java/view/GamePanel.java
@@ -2,6 +2,8 @@ package view;
 
 import javax.swing.JPanel;
 import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class GamePanel extends JPanel {
     public static final int ANCHO = 800;
@@ -11,5 +13,13 @@ public class GamePanel extends JPanel {
         super();
         setPreferredSize(new Dimension(ANCHO, ALTO));
         setFocusable(true);
+    }
+
+    //Def color fondo.
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0, ANCHO, ALTO);
     }
 }
