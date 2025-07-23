a4dbe0b801ea223a8bf772481b0b769db8390e6c
Eduardo <ewaldo.jefe@gmail.com>
2025-07-23 00:57:43 -0400

    Panel principal y ejecutable

--- src/main/java/launcher/JuegoNaves.java
+++ src/main/java/launcher/JuegoNaves.java
@@ -1,4 +1,21 @@
 package launcher;
 
+import view.GamePanel;
+
+import javax.swing.*;
+
 public class JuegoNaves {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Juegito FIUM!!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        GamePanel gamePanel = new GamePanel();
+        frame.add(gamePanel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+    }
 }
--- src/main/java/view/GamePanel.java
+++ src/main/java/view/GamePanel.java
@@ -1,4 +1,15 @@
 package view;
 
-public class GamePanel {
+import javax.swing.JPanel;
+import java.awt.Dimension;
+
+public class GamePanel extends JPanel {
+    public static final int ANCHO = 800;
+    public static final int ALTO = 600;
+
+    public GamePanel() {
+        super();
+        setPreferredSize(new Dimension(ANCHO, ALTO));
+        setFocusable(true);
+    }
 }
